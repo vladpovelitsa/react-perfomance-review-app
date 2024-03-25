24e64ce8b85954f24d39fc5fae0e811a09afe0d4
vladpovelitsa <vlad.povelitsa@gmail.com>
2024-03-25 20:31:32 +0200

    updated tailwind config

--- src/components/common/AppHeader.jsx
+++ src/components/common/AppHeader.jsx
@@ -1,6 +1,14 @@
 const AppHeader = () => (
   <>
-    <h1 className={'text-center text-3xl'}>AppHeader</h1>
+    <h1
+      className={
+        'font-playfair-display text-center text-3xl font-bold text-amber-950'
+      }
+    >
+      AppHeader
+    </h1>
+    <button className={'btn'}>Login</button>
+    <button className={'btn btn--accent'}>Registration</button>
   </>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-main': '#E5F4E3',
+        'brand-secondary': '#605856',
+        'brand-accent': '#1C6E8C',
+        'brand-strong': '#274156',
+        white: '#fff',
+      },
+    },
+    fontFamily: {
+      'playfair-display': ['Playfair Display', 'sans-serif'],
+      montserrat: ['Montserrat', 'serif'],
+    },
   },
   plugins: [],
-}
-
+};
